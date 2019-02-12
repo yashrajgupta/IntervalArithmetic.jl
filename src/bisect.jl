b97da19dcd95b0fe0ed1be74d042da9340f130b7
David Sanders <dpsanders@gmail.com>
2019-02-12 14:03:15 -0500

    Inline bisect

--- src/bisect.jl
+++ src/bisect.jl
@@ -7,7 +7,7 @@ const where_bisect = 0.49609375
 Split the interval `X` at position α; α=0.5 corresponds to the midpoint.
 Returns a tuple of the new intervals.
 """
-function bisect(X::Interval, α=where_bisect)
+@inline function bisect(X::Interval, α=where_bisect)
     @assert 0 ≤ α ≤ 1
 
     m = mid(X, α)
@@ -20,7 +20,7 @@ end
 
 Bisect the `IntervalBox` `X` at position α ∈ [0,1] along its longest side.
 """
-function bisect(X::IntervalBox, α=where_bisect)
+@inline function bisect(X::IntervalBox, α=where_bisect)
     i = argmax(diam.(X))  # find longest side
 
     return bisect(X, i, α)
@@ -31,7 +31,7 @@ end
 
 Bisect the `IntervalBox` in side number `i`.
 """
-function bisect(X::IntervalBox, i::Integer, α=where_bisect)
+@inline function bisect(X::IntervalBox, i::Integer, α=where_bisect)
 
     x1, x2 = bisect(X[i], α)
 
